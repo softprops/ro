5d0d80b7331d312cff334b9e781bbe2ec326f27b
softprops <d.tangren@gmail.com>
2015-02-11 01:07:15 -0500

    param builder thing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+#![feature(plugin)]
+#[plugin] #[no_link]
+extern crate json_macros;
+
+
+struct Params {
+  username: Option<String>,
+  loc: Option<(f64,f64)>,
+  link: Option<String>
+}
+
+impl Params {
+  fn empty() -> Params {
+    Params { username: None, loc: None, link: None }
+  }
+  fn loc(mut self, ll: (f64,f64)) -> Params {
+    self.loc = Some(ll);
+    self.link = None;
+    self
+  }
+  fn link(mut self, ln: &str) -> Params {
+    self.link = Some(ln.to_string());
+    self.loc = None;
+    self
+  }
+  fn username(mut self, user: &str) -> Params {
+    self.username = Some(user.to_string());
+    self
+  }
+}
+
 fn main() {
-    println!("Hello, world!");
+   let params = Params::empty().username("foo");
+   //println!("{}", json!({"username": params.username}).pretty().to_string())
 }
